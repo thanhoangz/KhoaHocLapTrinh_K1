2cb5bf262e2d77a242bc1487a88253d6f3ceb734
thanhoangz <xuanhoang.ks6@gmail.com>
2020-08-28 21:46:14 +0700

    Lesson 2

---
+++ Lesso2B/Lesso2B/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lesso2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dinhDanh = "";
+
+            Console.Write("Input: ");
+            dinhDanh =  Console.ReadLine();
+
+            // dog, cat
+
+            if ("dog" == dinhDanh) // không lên dùng dinhDanh == "dog"
+            {
+                Console.WriteLine("Go go go!");
+            }
+            else
+            {
+                if ("cat" == dinhDanh)
+                {
+                    Console.WriteLine("Meo meo meo!");
+                }
+                else
+                {
+                    Console.WriteLine("Khong phai cho cung khong phai meo");
+                }
+               
+            }
+
+        }
+    }
+}
---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, f;
+            Console.WriteLine("Input :");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+            f = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Tong: " + (a + b + c + d + e + f).ToString());
+
+
+            // 	72 79 65 78 71 => HOANG
+            // ý thứ 2 : in tên từ các chữ số được nhập từ bàn phím
+            string ketQua = ((char)a).ToString() + ((char)b).ToString() + ((char)c).ToString() + ((char)d).ToString() + ((char)e).ToString() + ((char)f).ToString();
+            Console.WriteLine(ketQua.ToString());
+
+        }
+    }
+}
